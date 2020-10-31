eea4370e72399b48ba383ab85c77a939a4e3ab67
pallovsky <pallovsky@gmail.com>
2020-10-31 11:12:35 +0100

    Created neccesary files for frontend. Configured Bootstrap for project.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CreateCampaignComponent } from './create-campaign/create-campaign.component';
+import { CampaignDetailsComponent } from './campaign-details/campaign-details.component';
+import { CampaignListComponent } from './campaign-list/campaign-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CreateCampaignComponent,
+    CampaignDetailsComponent,
+    CampaignListComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/campaign.ts
@@ -0,0 +1,10 @@
+export class Campaign {
+    id: number;
+    name: string;
+    keywords: string;
+    bid: number;
+    fund: number;
+    status: string;
+    town: string;
+    radius: number 
+}
