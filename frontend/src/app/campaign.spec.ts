ab64630506d8e40a2eb045468c7c2d5ad36db874
pallovsky <pallovsky@gmail.com>
2020-11-02 22:51:58 +0100

    Cleaning up. Removing unnecessary files.

--- frontend/src/app/campaign.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Campaign } from './campaign';
-
-describe('Campaign', () => {
-  it('should create an instance', () => {
-    expect(new Campaign()).toBeTruthy();
-  });
-});
--- frontend/src/app/emerald.spec.ts
+++
@@ -1,7 +0,0 @@
-import { Emerald } from './emerald';
-
-describe('Emerald', () => {
-  it('should create an instance', () => {
-    expect(new Emerald()).toBeTruthy();
-  });
-});
